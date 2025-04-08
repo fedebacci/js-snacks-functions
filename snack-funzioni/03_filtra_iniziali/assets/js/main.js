5bee53e29f232e0c43c472019114c3d6a1f673d4
fedebacci <federicobacci1999@gmail.com>
2025-04-08 16:50:59 +0200

    svolgimento terzo esercizio

---
+++ snack-funzioni/03_filtra_iniziali/assets/js/main.js
@@ -0,0 +1,39 @@
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+const searchedChar = "L"
+
+// Dichiara la funzione qui.
+// /**
+//  * Funzione che filtra un array
+//  * @param {string[]} namesList default = names - Array di stringhe da controllare
+//  * @param {string} char default = A - Lettera con cui devono iniziare le stringhe
+//  * @returns {string[]} stringhe che iniziano con la lettera scelta
+// */
+/**
+ * Funzione che filtra un array
+ * @param {array} namesList default = [] - Lista di elementi da filtrare
+ * @param {string} searchedInitial default = A - Iniziale per la quale filtrare
+ * @returns {string[]} Array filtrato
+*/
+function filterByInitials (namesList = [], searchedInitial = "A") {
+    const filteredNames = [];
+    
+    for (let i = 0; i < namesList.length; i ++) {
+        const currentName = namesList[i];
+        if (currentName[0].toLowerCase() === searchedInitial.toLowerCase()) {
+            filteredNames.push(currentName);
+        }
+    }
+
+    return filteredNames;
+};
+
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log("filterByInitials () con valori di default", filterByInitials ());
+console.log("filterByInitials (names, searchedChar)",  filterByInitials (names, searchedChar));
+console.log("filterByInitials (['Anna', 'Francesco'], 'B')", filterByInitials (['Anna', 'Francesco'], 'B'));
+
+
+//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
